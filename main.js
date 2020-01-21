807a93a5b76e005c945ad62de27d1835c2cd4569
Edith LE <edith.lop.estrada@gmail.com>
2020-01-21 15:17:37 -0600

    no a

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 let interval
-let frames
+let frames=0
+let logos = []
 
 const img = {
-    bg1: './images/bg-all.png',
-    kimberly: './sprite-one/merge_from_ofoct.png'
+    bg1: './images/edificios-ciudad-moderna-fondo-plano_1441-3122.jpg',
+    kimberly: './sprite-one/sprite-one-1.png',
+    instagram: './images/logo-instagram.png',
+    starbucks: './images/Starbucks-logo.png'
 }
 
 class Background{
-    constructor (x,y, src){
-        this.x = x
+    constructor (x,y){
+    this.x = x
     this.y = y
+    
     this.width = canvas.width
     this.height = canvas.height
     this.img = new Image()
-    this.img.src = src
+    this.img.src = img.bg1
        this.img.onload = () =>{
         this.draw()
         }
     }
     draw(){
+       // if(this.x >= 8487) this.x = 0
         if(this.x < -canvas.width) this.x = 0
         this.x --
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+        ctx.drawImage(
+                    this.img,
+                    this.x,
+                    this.y, 
+                    this.width, 
+                    this.height)
         ctx.drawImage(this.img, this.x + this.width, this.y, this.width, this.height)
     }    
 }
@@ -39,12 +49,13 @@ class Character{
         this.img = new Image()
         this.img.src = img.kimberly
         this.img.onload = () => {
+            this.x = x
             this.draw()
        }
     }
     draw(){
-        if(this.sx >= 1340) this.sx = 0
-//ctx.fillRect(this.x,this.y,10,10);
+        if(this.sx >= 670) this.sx = 0
+    //ctx.fillRect(this.x,this.y,10,10);
         ctx.drawImage(
             this.img,
             this.sx,
@@ -62,7 +73,7 @@ class Character{
         this.x += 10
         this.move()
     }
-    goLeft (){
+    goLeft(){
         this.x -=10
         this.move()
     }
@@ -75,23 +86,60 @@ class Character{
         this.move()
     }
     move (){
-        this.sx += 95
+        this.sx += 55
     }
 }
 
+class Logos{
+    constructor (x, y, imgSrc){
+        this.x = x
+        this.y = y
+        this.width = 100
+        this.height = 50
+        this.img = new Image()
+        this.img.src = imgSrc
+        this.img.onload = () =>{
+            this.draw()
+        }
+    }
+    draw(){
+        this.x --
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+
+}
+
 const kim = new Character (0, canvas.height - 100)
-const background = new Background(0, 0, img.bg1)
+const background = new Background(0, 0)
+
+function generateLog () {
+    let img, rnd
+    
+    if(frames % 100 === 0){
+        
+        rnd = Math.random() * canvas.height
+        if(Math.random()>= 0.5) img = img.instagram
+        if(Math.random ()>= 0,klckododflodfodfdfdw.5) img = img.starbucks
+        logos.push(new Logos(canvas.width + 100, rnd, img))
+    }
+}
+
+function drawLogos (){
+    generateLog()
+    logos.forEach(logos => logos.draw())
+}
 
 function startGame (){
     if (interval) return
-    interval = setInterval(update, 100/30)
+    interval = setInterval(update, 1000/60)
 }
 
 function update (){
     frames ++
-    ctx.clearRect(0,0, canvas.width, canvas.height)
-    kim.draw()  
+    ctx.clearRect(0,0, canvas.width, canvas.height) 
     background.draw()
+    drawLogos()
+    kim.draw() 
 }
 
 document.addEventListener('keydown', ({keyCode})=>{
